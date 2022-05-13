019a41b62b1220067552e2d0e59cae3f883d8a05
Jennifer <jennifer.zaminer@gmail.com>
2022-05-13 18:26:05 +0200

    Extracting Frequent Renter Points

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -23,12 +23,13 @@ class Customer {
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints ++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
+//            //determine amounts for each line
+//            // add frequent renter points
+//            frequentRenterPoints ++;
+//            // add bonus for a two day new release rental
+//            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
+//                frequentRenterPoints ++;
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getcharge()) + "\n";
             totalAmount += each.getcharge();
@@ -38,7 +39,7 @@ class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
-
+    
     
 }
 
